f0d56a2fd992dd27744089a0be45ab5b7bba5c6f
SarveshLimaye <sarveshlimaye2002@gmail.com>
2022-09-10 22:14:50 +0530

    Connected frontend with metamask :sparkles:

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Web3 from "web3";
+import detectEthereumProvider from "@metamask/detect-provider"
+import {useEffect,useState} from 'react';
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+    contract: null,
+  });
+  const [account, setAccount] = useState(null);
+  const setAccountListener = (provider) => {
+    provider.on("accountsChanged", (accounts) => setAccount(accounts[0]));
+  };
+useEffect(() => {
+  const loadProvider = async () => {
+    const provider = await detectEthereumProvider();
+    if (provider) {
+      setAccountListener(provider);
+      provider.request({ method: 'eth_requestAccounts' });
+      setWeb3Api({
+        web3: new Web3(provider),
+        provider,
+      });
+    } else {
+      console.log("Please install MetaMask!");
+    }
+  }
+
+  loadProvider();
+})
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>First dapp</h1>
+      <p>Account: {account}</p>
     </div>
   );
 }
