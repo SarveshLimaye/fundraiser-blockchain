bd0e060401b0031060fe7af330b31b11832fd7b4
SarveshLimaye <sarveshlimaye2002@gmail.com>
2022-09-11 09:54:09 +0530

    transfer fund done + smart contracts migrated on truffle :sparkles:

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Web3 from "web3";
 import detectEthereumProvider from "@metamask/detect-provider"
+import { loadContract } from "./utils/load-contract";
 import {useEffect,useState} from 'react';
 
 function App() {
@@ -10,18 +11,21 @@ function App() {
     contract: null,
   });
   const [account, setAccount] = useState(null);
+  const[balance,setBalance] = useState(null);
   const setAccountListener = (provider) => {
     provider.on("accountsChanged", (accounts) => setAccount(accounts[0]));
   };
 useEffect(() => {
   const loadProvider = async () => {
     const provider = await detectEthereumProvider();
+    const contract = await loadContract("Funder", provider);
     if (provider) {
       setAccountListener(provider);
       provider.request({ method: 'eth_requestAccounts' });
       setWeb3Api({
         web3: new Web3(provider),
         provider,
+        contract,
       });
     } else {
       console.log("Please install MetaMask!");
@@ -30,10 +34,26 @@ useEffect(() => {
 
   loadProvider();
 })
+
+useEffect(() => {
+  const loadBalance = async () => {
+    const { contract, web3 } = web3Api;
+    const balance = await web3.eth.getBalance(contract.address);
+    setBalance(web3.utils.fromWei(balance, "ether"));
+  };
+  web3Api.contract && loadBalance();
+}, [web3Api]);
+
+const tranferFund = async () => {
+  const { contract, web3 } = web3Api;
+ await contract.transfer({ from: account, value: web3.utils.toWei("2", "ether") });
+}
   return (
     <div className="App">
-      <h1>First dapp</h1>
+      <h1>Fund Raiser</h1>
       <p>Account: {account}</p>
+      {balance ? <p> Collected Amount : {balance} ETH</p> : <p>Loading...</p>}
+      <button onClick={tranferFund}>Transfer Fund</button>
     </div>
   );
 }
